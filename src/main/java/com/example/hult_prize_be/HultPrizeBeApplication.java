cae4070d84e0bc250ea77ccbfafded65a09f200d
why4832 <why48382@gmail.com>
2026-03-31 14:56:02 +0900

    init

---
+++ src/main/java/com/example/hult_prize_be/HultPrizeBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.hult_prize_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HultPrizeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HultPrizeBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hult_prize_be/HultPrizeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hult_prize_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HultPrizeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
