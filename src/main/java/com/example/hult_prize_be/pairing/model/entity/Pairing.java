4f19897bd6466cfdbebc0d459793cf56fad72f72
why4832 <why48382@gmail.com>
2026-04-09 14:12:33 +0900

    pairing 관계 잘 생각해서 부모와 보호자의 관계를 잘 연결하기

---
+++ src/main/java/com/example/hult_prize_be/pairing/model/entity/Pairing.java
@@ -0,0 +1,23 @@
+package com.example.hult_prize_be.pairing.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pairing {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pairingId;
+
+
+}
