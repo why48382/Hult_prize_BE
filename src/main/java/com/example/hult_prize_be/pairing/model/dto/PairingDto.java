57a4ab4c3ede669ec523f73e329b14e71ac47c5d
lee <why48382@gmail.com>
2026-04-11 14:05:47 +0900

    저장을 위한 메소드 구현과 저장 로직 구현

--- src/main/java/com/example/hult_prize_be/pairing/model/dto/PairingDto.java
+++ src/main/java/com/example/hult_prize_be/pairing/model/dto/PairingDto.java
@@ -1,4 +1,15 @@
 package com.example.hult_prize_be.pairing.model.dto;
 
+import com.example.hult_prize_be.member.model.entity.Members;
+import com.example.hult_prize_be.pairing.model.entity.Pairing;
+
 public class PairingDto {
+    public static class CreatReq {
+        public Pairing toEntity(Members elder, Members caregiver) {
+            return Pairing.builder()
+                    .elder(elder)
+                    .caregiver(caregiver)
+                    .build();
+        }
+    }
 }
--- src/main/java/com/example/hult_prize_be/pairing/service/PairingService.java
+++ src/main/java/com/example/hult_prize_be/pairing/service/PairingService.java
@@ -3,6 +3,8 @@ package com.example.hult_prize_be.pairing.service;
 import com.example.hult_prize_be.member.model.dto.MemberDto;
 import com.example.hult_prize_be.member.model.entity.Members;
 import com.example.hult_prize_be.member.repository.MemberRepository;
+import com.example.hult_prize_be.pairing.model.dto.PairingDto;
+import com.example.hult_prize_be.pairing.model.entity.Pairing;
 import com.example.hult_prize_be.pairing.repository.PairingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,7 +19,7 @@ public class PairingService {
     private final PairingRepository pairingRepository;
     private final MemberRepository memberRepository;
 
-    private record CodeEntry(String elderId, Instant expiresAt) {
+    private record CodeEntry(Members elderId, Instant expiresAt) {
     }
 
     private final ConcurrentHashMap<String, CodeEntry> codeStore = new ConcurrentHashMap<>();
@@ -31,26 +33,28 @@ public class PairingService {
             throw new RuntimeException("잘못된 역할입니다.");
         }
 
-        // 코드 생성을 메소드로 빼고 불러오는 방식으로 변경하기
+        // TODO 코드 생성을 메소드로 빼고 불러오는 방식으로 변경하기
         String code;
         do {
             code = String.format("%04d", new Random().nextInt(10000));
         } while (codeStore.containsKey(code)); // 이미 존재하면 다시 생성
-        codeStore.put(code, new CodeEntry(member.getMemberId(), Instant.now().plusSeconds(600)));
+        codeStore.put(code, new CodeEntry(member, Instant.now().plusSeconds(600)));
 
         return code;
     }
 
     public void codeVerify(MemberDto.AuthUser member, String inputCode) {
-        Members entity = memberRepository.findByMemberId(member.getMemberId()).orElseThrow(() -> new RuntimeException("사용자가 존재하지 않습니다"));
+        Members caregiver = memberRepository.findByMemberId(member.getMemberId()).orElseThrow(() -> new RuntimeException("사용자가 존재하지 않습니다"));
         CodeEntry entry = codeStore.get(inputCode);
         if (entry == null || Instant.now().isAfter(entry.expiresAt())) {
             codeStore.remove(inputCode);
+            throw new RuntimeException("만료되거나 잘못된 코드입니다.");
         }
-        // 여기까지 오면 검증 완료인것
-        // 이제 테이블에 저장하면 됨
-        // 그럼 jpa 부터 짜야지
         // TODO 이제 페어링 테이블 저장 로직만 만들면 됨
+        // 엔티티에 담기
+        PairingDto.CreatReq creatReq = new PairingDto.CreatReq();
+        Pairing pairing = creatReq.toEntity(entry.elderId, caregiver);
+        pairingRepository.save(pairing);
     }
 
 }
