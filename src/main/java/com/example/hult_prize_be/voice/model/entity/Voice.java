24abd8fe12b3df9ab86068188fc80467290db9b7
why4832 <why48382@gmail.com>
2026-04-13 18:42:34 +0900

    음성 요청 JPA 구현

---
+++ src/main/java/com/example/hult_prize_be/voice/model/entity/Voice.java
@@ -0,0 +1,59 @@
+package com.example.hult_prize_be.voice.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Voice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long voiceId;
+
+    private String originalText;
+
+    // GCS URL
+    private String audioUrl;
+
+    // 녹음 길이 (초)
+    private Integer audioDuration;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UrgencyLevel urgencyLevel;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "request", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<VoiceItems> items = new ArrayList<>();
+
+    public enum UrgencyLevel {
+        NORMAL,
+        URGENCY
+    }
+
+    public enum Status {
+        PENDING,
+        CONFIRMED,
+        CANCELED,
+        DONE
+        // 대기 중, 확인됨, 취소됨, 완료됨
+    }
+
+
+}
---
+++ src/main/java/com/example/hult_prize_be/voice/model/entity/VoiceItems.java
@@ -0,0 +1,28 @@
+package com.example.hult_prize_be.voice.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VoiceItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long voiceItemId;
+
+    @Column(nullable = false)
+    private String itemName;
+
+    @Column(nullable = false)
+    private Integer itemQuantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "voice_id", nullable = false)
+    private Voice voice;
+}
